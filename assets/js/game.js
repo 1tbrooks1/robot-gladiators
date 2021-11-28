f4f8d68781d14be829fb9ad20582899167be8745
tbrooks <tbrooks321@yahoo.com>
2021-11-28 16:23:34 -0500

    set variables and created fight sequence between player/program

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,26 +1,32 @@
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-// What is this?
+// You can also log multiple values at once like this 
+console.log(playerName, playerAttack, playerHealth);
 
-console.log(playerName);
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-console.log("This logs a string, good for leaving yourself a message");
+var fight = function() { 
+//Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
 
-// this will do math and log 20
+//Subtract the vlaue of playerAttack from the value of enemyHealth and use that resluts to update the value in the enemyHealth variable
+enemyHealth = enemyHealth - playerAttack;
 
-console.log(10 + 10);
+// Log a resulting message to the console so we know that it worked.
+console.log(
+    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+);
+// Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+playerHealth = playerHealth - enemyAttack;
 
-//What is this?
+// Log a resulting message to the console so we know that it worked.
+console.log(
+enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+);
+};
 
-console.log("Our robot's name is " + playerName);
-
-// Note the lack of qoutation marks around playerName window.alert(playerName);
-
-// this creates a function named "fight"//
-
-function fight () {
-    window.alert("The fight has begun!");
-
-}
-
-// fight ();
+fight();
